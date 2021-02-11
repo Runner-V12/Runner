d800226fcebb24b0b93ebd6bc7f1d8d5b371d644
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2021-02-11 15:25:03 +0100

    Add JumpTrigger
    Add a base abstract class Trigger for all common logic

---
+++ Runner/Assets/Scripts/Triggers/JumpTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : Trigger
+{
+        [SerializeField][Tooltip("Scaled 100 times for simplicity")] private float jumpForce;
+
+        private void Awake()
+        {
+                jumpForce *= 100;
+        }
+
+        protected override void Action(GameObject collided)
+        {
+                var body = collided.GetComponent<Rigidbody2D>();
+                body.AddForce(new Vector2(0f, jumpForce));
+        }
+}
---
+++ Runner/Assets/Scripts/Triggers/Trigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Trigger : MonoBehaviour
+{
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+                Action(collision.gameObject);
+        }
+
+        protected abstract void Action(GameObject collided);
+}
