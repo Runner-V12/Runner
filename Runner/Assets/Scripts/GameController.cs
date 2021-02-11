b2795ea7f97b3463a0024d3c6f5b41708b8f4310
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2021-02-11 16:31:24 +0100

    Add Restart button
    Rename PauseControl to GameController

---
+++ Runner/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+        public bool paused = false;
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private string pausedText = "Play";
+        [SerializeField] private string playingText = "Pause";
+
+        public void Awake()
+        {
+                if (paused)
+                {
+                        Pause();
+                }
+        }
+
+        public void Pause()
+        {
+                Time.timeScale = 0;
+                paused = true;
+                pauseButton.GetComponentInChildren<Text>().text = pausedText;
+        }
+
+        public void Play()
+        {
+                Time.timeScale = 1;
+                paused = false;
+                pauseButton.GetComponentInChildren<Text>().text = playingText;
+        }
+
+        public void Toggle()
+        {
+                if (paused == false)
+                {
+                        Pause();
+                } else if (paused == true)
+                {
+                        Play();
+                }
+        }
+
+        public void Restart()
+        {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                Pause();
+        }
+}
---
+++ Runner/Assets/Scripts/Triggers/TriggerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TriggerController : MonoBehaviour
+{
+        [SerializeField] private GameObject trigger;
+
+        private void Update()
+        {
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                                CreateTrigger(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                        } else if (Input.GetMouseButtonDown(1))
+                        {
+                                DeleteTrigger(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                        }
+                }
+        }
+
+        private void CreateTrigger(Vector3 mousePosition)
+        {
+                mousePosition.z = 0;
+                Instantiate(trigger, mousePosition, Quaternion.identity);
+        }
+
+        private void DeleteTrigger(Vector3 mousePosition)
+        {
+
+        }
+}
