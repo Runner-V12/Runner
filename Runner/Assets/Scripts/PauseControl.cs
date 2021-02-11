d24943e6aa1f0ac4c584e82fb6e1d07be92e0137
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2021-02-11 15:46:26 +0100

    Add PauseController

---
+++ Runner/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+        public bool paused = false;
+
+        public void Pause()
+        {
+                Time.timeScale = 0;
+                paused = true;
+        }
+
+        public void Play()
+        {
+                Time.timeScale = 1;
+                paused = false;
+        }
+
+        public void Toggle()
+        {
+                Debug.Log("toggle pause");
+                if (paused == false)
+                {
+                        Pause();
+                } else if (paused == true)
+                {
+                        Play();
+                }
+        }
+}
