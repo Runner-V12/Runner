a88b17bced4fed8924556fa18a7eaaf94068964e
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2021-02-11 14:52:17 +0100

    Add CameraFollow

---
+++ Runner/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+        [SerializeField] private Transform target;
+        [SerializeField] private float smoothSpeed = 0.125f;
+        [SerializeField] private Vector3 offset;
+
+        void FixedUpdate()
+        {
+                var desiredPosition = target.position + offset;
+                var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+                transform.position = smoothedPosition;
+
+                transform.LookAt(target);
+        }
+}
