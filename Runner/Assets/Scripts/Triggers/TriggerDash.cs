fce94242b8f341e3e58a6c0508cc38780c5e78e0
chiliwa <thibault.lecointe@epitech.eu>
2021-02-13 03:22:30 +0100

    Add Dash trigger

---
+++ Runner/Assets/Scripts/Triggers/TriggerDash.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDash : Trigger
+{
+    protected override void Action(GameObject collided)
+    {
+        collided.GetComponent<PlayerController>().Dash();
+    }
+}
