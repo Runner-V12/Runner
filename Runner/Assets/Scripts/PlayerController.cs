32ba200c125675143d438a68b75b65cff7773e3f
chiliwa <thibault.lecointe@epitech.eu>
2021-02-19 14:24:22 +0100

    Update PlayerController.cs

--- Runner/Assets/Scripts/PlayerController.cs
+++ Runner/Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
         {
             dash = false;
         }
-        if (speed != 0f)
+        if (m_Rigidbody2D.velocity.x != 0f)
             GameObject.FindGameObjectWithTag("Background").GetComponent<SpriteRenderer>().material.SetFloat("Vector1_BD96FC95",0.2f);
         else
             GameObject.FindGameObjectWithTag("Background").GetComponent<SpriteRenderer>().material.SetFloat("Vector1_BD96FC95",0f);
@@ -92,14 +92,10 @@ public class PlayerController : MonoBehaviour
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
-            try
-            {
+            if(speed != 0f)
                 m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);    
-            }
-            catch (System.Exception)
-            {
-            }
-            
+            else
+                m_Rigidbody2D.velocity = new Vector3(0f,m_Rigidbody2D.velocity.y);
             // If the input is moving the player right and the player is facing left...
             if (move > 0 && !m_FacingRight)
             {
