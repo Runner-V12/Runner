38607366e052f9c3b25c22ddabfcb5d5e1717996
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2021-02-21 17:44:50 +0100

    Add jump particles

--- Runner/Assets/Scripts/Triggers/JumpTrigger.cs
+++ Runner/Assets/Scripts/Triggers/JumpTrigger.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class JumpTrigger : Trigger
 {
+    [SerializeField] private GameObject jumpParticles;
     protected override void Action(GameObject collided)
     {
         collided.GetComponent<PlayerController>().Jump();
+        Destroy(Instantiate(jumpParticles, collided.transform.position, Quaternion.identity), 2);
     }
 }
