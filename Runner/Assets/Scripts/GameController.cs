35799dab2d03464dd22246a27589e5850b57f29d
Tydyus <tydyus@gmail.com>
2021-02-22 12:36:27 +0100

    Revert "Update GameController.cs"
    This reverts commit 1bbffa6a5d39044866d7d74cdf7124a10f888aed.

--- Runner/Assets/Scripts/GameController.cs
+++ Runner/Assets/Scripts/GameController.cs
@@ -45,7 +45,7 @@ public class GameController : MonoBehaviour
             {
                 if (item.TryGetComponent<Button>(out Button button)) button.enabled = false;
                 if (item.TryGetComponent<Image>(out Image image)) image.enabled = false;
-                if (item.TryGetComponent<TextMeshProUGUI>(out TextMeshProUGUI text)) text.enabled = false;
+                if (item.TryGetComponent<TextMeshPro>(out TextMeshPro text)) text.enabled = false;
                     
             }
             foreach (GameObject item in EditUI)
@@ -69,8 +69,7 @@ public class GameController : MonoBehaviour
             {
                 if (item.TryGetComponent<Button>(out Button button)) button.enabled = true;
                 if (item.TryGetComponent<Image>(out Image image)) image.enabled = true;
-                if (item.TryGetComponent<TextMeshProUGUI>(out TextMeshProUGUI text)) text.enabled = true;
-                
+                if (item.TryGetComponent<TextMeshPro>(out TextMeshPro text)) text.enabled = true;
 
             }
             foreach (GameObject item in EditUI)
