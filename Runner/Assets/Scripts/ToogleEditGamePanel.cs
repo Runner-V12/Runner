f14ff10ff42497032f7ca77145fcc807cff0d669
chiliwa <thibault.lecointe@epitech.eu>
2021-02-25 20:20:29 +0100

    Add transition

---
+++ Runner/Assets/Scripts/ToogleEditGamePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToogleEditGamePanel : MonoBehaviour
+{
+    [SerializeField] private GameController _gameController;
+    private float _gravity;
+
+    public void toogle() {
+        _gameController.ToogleEditing();
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().simulated = false;
+    }
+
+    public void desactivate() {
+        this.gameObject.SetActive(false);
+        if (!_gameController.editMode)
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().simulated = true;
+    }
+}
---
+++ Runner/Assets/Scripts/endlvl_2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class endlvl_2 : MonoBehaviour
+{
+    [SerializeField]private string NextLvlName;
+    [SerializeField]private Text PanelLevelName;
+    [SerializeField]private GameObject _Panel;
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        _Panel.SetActive(true);
+        PanelLevelName.text = SceneManager.GetActiveScene().name;
+    }
+
+    public void changeLVL() {
+        SceneManager.LoadScene(NextLvlName,LoadSceneMode.Single);
+    }
+}
